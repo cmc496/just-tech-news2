cf79812f5530a2d856c62db0a33f4a31798f7ad5
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-06-07 19:26:01 -0400

    14.1.6

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
 router.use((req, res) => {
     res.status(404).end();
 });
+router.use('/', homeRoutes);
 
 module.exports = router;
