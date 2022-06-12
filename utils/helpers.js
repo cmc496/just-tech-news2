057dddd799788e072ed563ebcd4edb68500d7aac
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-06-12 10:50:10 -0400

    partials implemented

---
+++ utils/helpers.js
@@ -0,0 +1,25 @@
+// export function that makes test in __tests__/helpers.test pass
+
+module.exports = {
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+            date
+        ).getFullYear()}`;
+    },
+    format_plural: (word, amount) => {
+        if (amount !== 1) {
+            return `${word}s`;
+        }
+        return word;
+    },
+    format_url: url => {
+        return url
+            .replace('http://', '')
+            .replace('https://', '')
+            .replace('www.', '')
+            .split('/')[0]
+            .split('?')[0];
+    }
+}
+
+// (before running test, import format_date() function into helpers.test.js)
